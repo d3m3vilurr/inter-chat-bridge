dc55536be103dd0f3cbdc8eb8aac7f59a6411939
Sunguk Lee <d3m3vilurr@gmail.com>
2016-05-26 14:12:45 +0900

    slack: To send message support link for room and user
    - Add more regex rules and fix room match rule
    - Now can send `<` and `>` chars

--- channels/slack.py
+++ channels/slack.py
@@ -1,13 +1,34 @@
 from slackclient import SlackClient
 import re
 import HTMLParser
+import cgi
 from channels import Channel, Room
 
-USER_REGEX = re.compile('(<@([a-zA-Z0-9]+)(|[^>]+)?>)')
-ROOM_REGEX = re.compile('(<#([_-a-zA-Z0-9]+)(|[^>]+)?>)')
-URL_REGEX = re.compile('(<([^>|]+)(|[^>]+)?>)')
+SEND_USER_REGEX = re.compile('(@([a-zA-Z0-9]+))')
+SEND_ROOM_REGEX = re.compile('(#([_\-a-zA-Z0-9]+))')
+RECV_USER_REGEX = re.compile('(<@([a-zA-Z0-9]+)(|[^>]+)?>)')
+RECV_ROOM_REGEX = re.compile('(<#([_\-a-zA-Z0-9]+)(|[^>]+)?>)')
+RECV_URL_REGEX = re.compile('(<([^>|]+)(|[^>]+)?>)')
 
+escape = cgi.escape
 unescape = HTMLParser.HTMLParser().unescape
+
+def to_send_format(msg, pattern, process):
+    tmp = msg
+    buf = []
+    while True:
+        s = pattern.split(tmp, maxsplit=1)
+        if not len(s):
+            break
+        if len(s) == 1:
+            buf.append(s[0])
+            break
+        r = process(s[2])
+        if r != s[1]:
+            r = '<' + r + '>'
+        buf += [s[0], r]
+        tmp = s[3]
+    return ''.join(buf)
 
 class SlackRoom(Room):
 
@@ -17,6 +38,21 @@ class SlackRoom(Room):
         self.lastid = None
         self.queue = []
 
+    def find_userid(self, username):
+        _ = self.slack.client.server.users.find(username)
+        return '@' + (_ and _.id or username)
+    def find_username(self, userid):
+        _ = self.slack.client.server.users.find(userid)
+        return '@' + (_ and _.name or userid)
+
+    def find_roomid(self, roomname):
+        _ = self.slack.client.server.channels.find(roomname)
+        return '#' + (_ and _.id or roomname)
+
+    def find_roomname(self, roomid):
+        _ = self.slack.client.server.channels.find(roomid)
+        return '#' + (_ and _.name or roomid)
+
     def fetch_messages(self):
         self.slack.fetch_messages()
         return super(SlackRoom, self).fetch_messages()
@@ -25,17 +61,13 @@ class SlackRoom(Room):
         user = self.slack.client.server.users.find(sender)
         if user:
             sender = user.name
-        for t, m, _ in USER_REGEX.findall(message):
-            _ = self.slack.client.server.users.find(m)
-            if _:
-                m = '@' + _.name
+        for t, m, _ in RECV_USER_REGEX.findall(message):
+            m = self.find_username(m)
             message = message.replace(t, m)
-        for t, c, _ in ROOM_REGEX.findall(message):
-            _ = self.slack.client.server.channels.find(c)
-            if _:
-                c = '#' + _.name
+        for t, c, _ in RECV_ROOM_REGEX.findall(message):
+            c = self.find_roomname(c)
             message = message.replace(t, c)
-        for t, u, a in URL_REGEX.findall(message):
+        for t, u, a in RECV_URL_REGEX.findall(message):
             if orig_data.get('subtype'):
                 if orig_data['subtype'] == 'file_share' and \
                         orig_data.get('file'):
@@ -48,6 +80,10 @@ class SlackRoom(Room):
         self.queue.append((sender, message))
 
     def send_message(self, sender, message):
+        message = escape(message)
+        message = to_send_format(message, SEND_USER_REGEX, self.find_userid)
+        message = to_send_format(message, SEND_ROOM_REGEX, self.find_roomid)
+        #print 'DEBUG_SLACK_SEND_MSG', message
         self.slack.client.rtm_send_message(self.channel.id, message)
 
 class Slack(Channel):
@@ -90,14 +126,14 @@ class Slack(Channel):
                     room = self.rooms.get(msg['channel'])
                     if not room:
                         continue
-                    #print 'DEBUG_SLACK_MSG', msg
+                    #print 'DEBUG_SLACK_RECV_MSG', msg
                     user = msg.get('user') or msg.get('username') or \
                            msg.get('comment', {}).get('user') or '_'
                     if user == self.ignore:
                         continue
                     room.append_message(user, msg.get('text', ''), msg)
                 else:
-                    print 'UNKNOWN_SLACK_MSG', msg
+                    print 'UNKNOWN_SLACK_RECV_MSG', msg
 
     def close(self):
         pass
